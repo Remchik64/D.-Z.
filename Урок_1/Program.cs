9c5d7c2ab903f15682dde4660da7752f25f46fe2
Renat <remch2013ya.ru>
2023-10-02 18:51:22 +0400

    Задание 1

---
+++ Урок_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, который принимает на вход два дробных числа (A и B) и возводит число A
+//  в натуральную степень B.
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите A: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Введите B: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine(Math.Pow(a, b));
+    }
+}
