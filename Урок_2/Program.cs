c83f5cf0184d1a8d8eac8a90899172b085e2677a
Renat <remch2013ya.ru>
2023-10-02 19:02:19 +0400

    Задание 2

---
+++ Урок_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+
+ int num = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        while (num > 0)
+        {
+            sum += num % 10; // Add the last digit of the number to the sum. 
+
+            num /= 10; // Remove the last digit of the number. 
+
+        }
+
+        Console.WriteLine($"Сумма числа равна: {sum}");
