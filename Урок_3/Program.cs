3607dd407d6b8aaa06a0dcf3aac1948bf74a2476
Renat <remch2013ya.ru>
2023-10-02 22:47:07 +0400

    Задание 2.3

---
+++ Урок_3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Задача 29: Напишите программу, которая задаёт массив из рандомных 8 элементов и выводит их на экран в квадратных скобках.
+
+  Random rnd = new Random();
+    int[] arr = new int[8];
+    for (int i = 0; i < 8; i++)
+        arr[i] = rnd.Next(0, 20);
+
+    Console.Write("[");
+    for (int i = 0; i < 8; i++)
+        Console.Write(arr[i] + " ");
+
+    Console.WriteLine("]");
